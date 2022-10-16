f93849d130795037a1d3c1d0ed7db115e3f0dd62
ycchu8992 <ycchu8992@gmail.com>
2022-10-16 11:05:49 +0800

    proj03_01 complete

--- Lab2/proj03_01/powerlawTransform.m
+++ Lab2/proj03_01/powerlawTransform.m
@@ -1,7 +1,5 @@
-function [output] = powerlawTransform(inputImage,c,r)
-%POWERLAWTRANSFORM Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [outputImage] = powerlawTransform(inputImage,c,r)
+   temp = c*(inputImage.^r);
+   outputImage = temp;
 end
 
