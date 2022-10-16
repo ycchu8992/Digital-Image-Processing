43581de3b4604a3ae109089a562232c6cde7dc27
ycchu8992 <ycchu8992@gmail.com>
2022-10-16 17:15:42 +0800

    change image in proj03_01

--- Lab2/proj03_01/main.m
+++ Lab2/proj03_01/main.m
@@ -3,7 +3,7 @@ clear;
 close all;
 
 %read the image and change its type so that all pixels' intensity are in double
-inputImage = double(imread("Fig.tif"));
+inputImage = double(imread("Fig0308(a)(fractured_spine).tif"));
 
 c = 255/log(256);
 
@@ -26,7 +26,7 @@ figure(2);
 subplot(2,3,1);imshow(uint8(inputImage));title("Original");
 
 for i = 0:1:4
-    r = 0.3+0.3*i;
+    r = 0.3+0.1*i;
     c = 255/255^r;
     gammaValue = sprintf("%.1f",r);
     [output] = powerlawTransform(inputImage,c,r);
