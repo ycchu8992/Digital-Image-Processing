73811c269611545c69c7dc6f007dc49af33af3fb
ycchu8992 <ycchu8992@gmail.com>
2022-10-16 17:21:42 +0800

    change image and folder

--- Lab2/proj03_01/main.m
+++ Lab2/proj03_01/main.m
@@ -18,20 +18,29 @@ subplot(1,2,1);imshow(im2single(uint8(inputImage)));title("Original");
 subplot(1,2,2);imshow(outputImage);title("Log transformation");
 
 filename = "LogTransformation.jpg";
-imwrite(outputImage,filename);
 
+imwrite(outputImage,filename);
 
 figure(2);
 
 subplot(2,3,1);imshow(uint8(inputImage));title("Original");
 
 for i = 0:1:4
+
     r = 0.3+0.1*i;
+    
     c = 255/255^r;
+    
     gammaValue = sprintf("%.1f",r);
+    
     [output] = powerlawTransform(inputImage,c,r);
+    
     outputImage = im2single(uint8(output));
+    
     subplot(2,3,i+2);imshow(outputImage);title("powerLaw:gamma = " + gammaValue);
+    
     filename = "powerLawWithGamma_"+gammaValue+".jpg";
+    
     imwrite(outputImage,filename);
+
 end
